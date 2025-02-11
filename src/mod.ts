70acdc9500a0e91337354f8204b659c47f1ac213
Jon <ruisong20@gmail.com>
2025-02-11 16:20:46 +0800

    [release] update package name and enhance exports in mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,6 @@
+// Keypair
 export { Ed25519Keypair } from "./key/keypair.ts"
+
+// Cryptographic suites
 export { EddsaRdfc2022 } from "./suite/rdfc.ts"
+export { EddsaJcs2022 } from "./suite/jcs.ts"
